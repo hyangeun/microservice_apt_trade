c6d93cb07d0fb47ea536709032e706eda8f271d6
지향은 <eun@jihyang-eun-ui-iMac.local>
2020-01-21 11:17:02 +0900

    updated

--- aptTrade-frontend/src/main/java/com/microservice/apt/address/service/AddressFeignConfiguration.java
+++ aptTrade-frontend/src/main/java/com/microservice/apt/address/service/AddressFeignConfiguration.java
@@ -2,6 +2,7 @@ package com.microservice.apt.address.service;
 
 import feign.Client;
 import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.netflix.feign.FeignClientsConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,13 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import(FeignClientsConfiguration.class)
 public class AddressFeignConfiguration {
+
+    @Value("${feign.post-api.username:}")
+    private String username;
+
+    @Value("${feign.post-api.password:}")
+    private String password;
+
     @Bean
     public Client feignClient() {
         return new Client.Default(null, null);
@@ -17,7 +25,7 @@ public class AddressFeignConfiguration {
 
     @Bean
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
-        return new BasicAuthRequestInterceptor("findAddr", "findAddr");
+        return new BasicAuthRequestInterceptor(username, password);
     }
 
 }
--- aptTrade-frontend/src/main/java/com/microservice/apt/olv/service/OlvFeignConfiguration.java
+++ aptTrade-frontend/src/main/java/com/microservice/apt/olv/service/OlvFeignConfiguration.java
@@ -2,6 +2,7 @@ package com.microservice.apt.olv.service;
 
 import feign.Client;
 import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.netflix.feign.FeignClientsConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,13 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import(FeignClientsConfiguration.class)
 public class OlvFeignConfiguration {
+
+    @Value("${feign.olv-api.username:}")
+    private String username;
+
+    @Value("${feign.olv-api.password:}")
+    private String password;
+
     @Bean
     public Client feignClient() {
         return new Client.Default(null, null);
@@ -17,7 +25,7 @@ public class OlvFeignConfiguration {
 
     @Bean
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
-        return new BasicAuthRequestInterceptor("findAddr", "findAddr");
+        return new BasicAuthRequestInterceptor(username, password);
     }
 
 }
